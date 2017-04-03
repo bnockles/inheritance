81891a5577a8d718c7b81aabf44474dd6ed09927
Student8 <Student8@10.8.33.172>
2017-04-03 14:01:48 -0400

    added club

--- Inheritance/src/inheritance/TraceyKong.java
+++ Inheritance/src/inheritance/TraceyKong.java
@@ -5,7 +5,7 @@ public class TraceyKong extends ClubMember {
 	public TraceyKong() {
 		super("Tracey", "Kong", 2017, Main.COMP_SCI_MAJOR, "k-drama");
 	}
-	 
+	  
 	public void act(){
 		while(true){
 		super.act();
