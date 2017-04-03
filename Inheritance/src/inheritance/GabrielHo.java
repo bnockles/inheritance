eb305da82705acc2bb148a9211311fe06cd562b6
Student 8 <Student 8@10.8.33.173>
2017-04-03 13:49:17 -0400

    yes

--- Inheritance/src/inheritance/GabrielHo.java
+++ Inheritance/src/inheritance/GabrielHo.java
@@ -5,12 +5,6 @@ public class GabrielHo extends Student {
 	public GabrielHo() {
 		super("Gabriel", "Ho", 2017, Main.COMP_SCI_MAJOR);
 	}
-	/**
-	public GabrielHo(String name, String surname, int gradClass, Major major) {
-		super(name, surname, gradClass, major);
-		// TODO Auto-generated constructor stub
-	}
-	**/
 	
 	public void act(){
 		stateName();
