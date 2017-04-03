15545b5d73879a76f27ff22423727caa82a4ab86
Student 8 <Student 8@10.8.33.158>
2017-04-03 13:59:41 -0400

    Added Jenniber Club

--- Inheritance/src/inheritance/JenniberFranco.java
+++ Inheritance/src/inheritance/JenniberFranco.java
@@ -1,9 +1,9 @@
 package inheritance;
 
-public class JenniberFranco extends Student {
+public class JenniberFranco extends ClubMember {
 
 	public JenniberFranco() {
-		super("Jenniber", "Franco", 2017, Main.COMP_SCI_MAJOR);
+		super("Jenniber", "Franco", 2017, Main.COMP_SCI_MAJOR, "K-Drama");
 	}
 	
 	public void act(){
