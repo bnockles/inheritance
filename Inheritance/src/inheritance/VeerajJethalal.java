c992fe752ed677ff42c47e17bbf3e2a386d6b7ab
Veeraj <Period82.0@10.8.33.160>
2017-04-03 13:50:11 -0400

    Veeraj

--- Inheritance/src/inheritance/VeerajJethalal.java
+++ Inheritance/src/inheritance/VeerajJethalal.java
@@ -5,7 +5,7 @@ public class VeerajJethalal extends Student{
 	public VeerajJethalal() {
 		super("Veeraj", "Jethalal", 2017, Main.COMP_SCI_MAJOR);
 	}
-	
+	//
 	@Override
 	public void act(){
 		stateName();
