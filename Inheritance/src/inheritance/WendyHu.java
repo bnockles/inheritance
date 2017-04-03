2e8009f4be95aab93ae8469eb035e425478c0cd0
Student6 <Student6@10.8.33.136>
2017-04-03 13:47:12 -0400

    Added Wendy Hu

---
+++ Inheritance/src/inheritance/WendyHu.java
@@ -0,0 +1,15 @@
+package inheritance;
+
+public class WendyHu extends Student {
+
+	public WendyHu() {
+		// TODO Auto-generated constructor stub
+		super("Wendy","Hu",2021,Main.COMP_SCI_MAJOR);
+	}
+	
+	@Override 
+	public void act(){
+		stateName();
+	}
+
+}
