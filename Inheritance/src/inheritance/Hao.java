78172e2407ef064345af5e469b2e927e1d8d3a10
Student8 <Student8@10.8.33.161>
2017-04-03 13:50:00 -0400

    added hao

---
+++ Inheritance/src/inheritance/Hao.java
@@ -0,0 +1,12 @@
+package inheritance;
+
+public class Hao extends Student {
+
+	public Hao() {
+		super("Hao", "Yuan", 2017, Main.COMP_SCI_MAJOR);
+	}
+	
+	public void act() {
+		super.act();	
+	}
+}
