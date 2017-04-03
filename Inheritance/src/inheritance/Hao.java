11623f2fbc0a7ca553f0aa9573e57f49b6f4f4f7
Student8 <Student8@10.8.33.161>
2017-04-03 13:55:25 -0400

    hao

--- Inheritance/src/inheritance/Hao.java
+++ Inheritance/src/inheritance/Hao.java
@@ -2,7 +2,7 @@ package inheritance;
 
 public class Hao extends Student {
 
-	public Hao() {
+	public Hao(String name, String surname, int gradClass, Major major) {
 		super("Hao", "Yuan", 2017, Main.COMP_SCI_MAJOR);
 	}
 	
