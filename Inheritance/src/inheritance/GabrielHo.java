0b16c23255e898a8c4c2d34e15fbe697171d7d63
Student 8 <Student 8@10.8.33.173>
2017-04-03 13:49:39 -0400

    yes

--- Inheritance/src/inheritance/GabrielHo.java
+++ Inheritance/src/inheritance/GabrielHo.java
@@ -5,7 +5,7 @@ public class GabrielHo extends Student {
 	public GabrielHo() {
 		super("Gabriel", "Ho", 2017, Main.COMP_SCI_MAJOR);
 	}
-	
+	//yes
 	public void act(){
 		stateName();
 		stateStatus();
