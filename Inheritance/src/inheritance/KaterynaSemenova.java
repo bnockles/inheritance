85e084989361cd97e0072b74d68eedd1dfa4de40
Student 8 <Student 8@10.8.33.175>
2017-04-03 13:59:37 -0400

    club

--- Inheritance/src/inheritance/KaterynaSemenova.java
+++ Inheritance/src/inheritance/KaterynaSemenova.java
@@ -1,9 +1,9 @@
 package inheritance;
 
-public class KaterynaSemenova extends Student {
+public class KaterynaSemenova extends ClubMember {
 
 	public KaterynaSemenova(){
-		super("Kat","Semenova", 2012, Main.COMP_SCI_MAJOR);
+		super("Kat","Semenova", 2012, Main.COMP_SCI_MAJOR, "music");
 	}
 	
 	public void act(){
