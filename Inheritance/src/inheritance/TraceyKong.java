aafc68cf9f24ab51c757eb949e5ca662f236e44d
Student8 <Student8@10.8.33.172>
2017-04-03 13:55:48 -0400

    added Tracey kong

--- Inheritance/src/inheritance/TraceyKong.java
+++ Inheritance/src/inheritance/TraceyKong.java
@@ -5,15 +5,9 @@ public class TraceyKong extends Student {
 	public TraceyKong() {
 		super("Tracey", "Kong", 2017, Main.COMP_SCI_MAJOR);
 	}
-
-	public TraceyKong(String name, String surname, int gradClass, Major major){
-		super(name, surname, gradClass, major);
-	}
 	
 	public void act(){
-		while(true){
-			super.act();
-		}
+		super.act();
 	}
 
 }
