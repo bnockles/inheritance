07664fbb43c0dc2fcd202ae3eb492bb71b7b54fd
Student 8 <Student 8@10.8.33.171>
2017-04-03 13:48:19 -0400

    Richard

---
+++ Inheritance/src/inheritance/RichardLy.java
@@ -0,0 +1,18 @@
+package inheritance;
+
+public class RichardLy extends Student {
+
+	public RichardLy() {
+		// TODO Auto-generated constructor stub
+		super("Richard", "Ly", 2017, Main.COMP_SCI_MAJOR);
+	}
+
+//	public RichardLy(String name, String surname, int gradClass, Major major) {
+//		super(name, surname, gradClass, major);
+//		// TODO Auto-generated constructor stub
+//	}
+	public void act(){
+		stateName();
+		stateStatus();
+	}
+}
