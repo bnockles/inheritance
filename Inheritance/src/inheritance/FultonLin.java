944762c1cf97b005a509a25332f1611376599a81
Student8 <Student8@10.8.33.105>
2017-04-03 13:46:54 -0400

    asd

---
+++ Inheritance/src/inheritance/FultonLin.java
@@ -0,0 +1,12 @@
+package inheritance;
+
+public class FultonLin extends Student{
+
+	public FultonLin() {
+		super("Fulton", "Lin", 2017, Main.COMP_SCI_MAJOR);
+	}
+
+	public void act(){
+		super.act();
+	}
+}
