d9817d77d27c5c402aa8cd1f800bcfaf556a7169
Student 6 <Student 6@10.8.33.100>
2017-04-04 12:21:03 -0400

    Matthew Nguyen

--- Inheritance/src/inheritance/MatthewNguyen.java
+++ Inheritance/src/inheritance/MatthewNguyen.java
@@ -1,9 +1,9 @@
 package inheritance;
 
-public class MatthewNguyen extends Student {
+public class MatthewNguyen extends ClubMember {
 
 	public MatthewNguyen() {
-		super("Matthew", "Nguyen", 2017, Main.COMP_SCI_MAJOR);
+		super("Matthew", "Nguyen", 2017, Main.COMP_SCI_MAJOR, "pokemon");
 	}
 	
 	public void act(){
