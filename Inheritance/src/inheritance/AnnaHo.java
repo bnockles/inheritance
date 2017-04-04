31d06ad6931cb7878c978db7fe4b86cf66b51328
Student6 <Student6@10.8.33.161>
2017-04-04 12:22:36 -0400

    anna

--- Inheritance/src/inheritance/AnnaHo.java
+++ Inheritance/src/inheritance/AnnaHo.java
@@ -3,11 +3,12 @@ package inheritance;
 public class AnnaHo extends ClubMember{
 	
 	public AnnaHo() {
-		super("Anna", "Ho", 2017, Main.COMP_SCI_MAJOR, "supergirl");
+		super("Anna", "Ho", 2017, Main.COMP_SCI_MAJOR, "batman");
 
 	} 
 	 
 	public void act(){
-		super.act();
+		//super.act();
+		System.out.println("Anna Ho");
 	}
 }
