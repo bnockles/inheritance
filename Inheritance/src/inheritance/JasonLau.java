a211a61a0f25c79091806edbe0bd9718f6d7eea2
Student6 <Student6@10.8.33.157>
2017-04-04 12:20:19 -0400

    pooosh

--- Inheritance/src/inheritance/JasonLau.java
+++ Inheritance/src/inheritance/JasonLau.java
@@ -1,12 +1,12 @@
 package inheritance;
 
-public class JasonLau extends Student{
+public class JasonLau extends ClubMember{
 	
 	public JasonLau(){
-		super("Jason","Lau",2017,Main.COMP_SCI_MAJOR);
+		super("Jason","Lau",2017,Main.COMP_SCI_MAJOR,"roblox");
 	}
 	
 	public void act(){
-		System.out.println("JLau");
+		System.out.println("ayyy its ya buoy JLau");
 	}
 }
