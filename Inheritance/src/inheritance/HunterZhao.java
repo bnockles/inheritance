83a20fcfeb86d7b1d498b43340ec0dd24328f16d
student 6 <student 6@10.8.33.151>
2017-04-04 12:20:14 -0400

    start

--- Inheritance/src/inheritance/HunterZhao.java
+++ Inheritance/src/inheritance/HunterZhao.java
@@ -1,9 +1,9 @@
 package inheritance;
 
-public class HunterZhao extends Student {
+public class HunterZhao extends ClubMember {
 
 	public HunterZhao() {
-		super();
+		super("Hunter", "Zhao", 2017, Main.COMP_SCI_MAJOR, "minecraft");
 	}
 	
 	public void act(){
