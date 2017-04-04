93290ecb12f0d4c3da6dd86e70143401871d201a
Student8 <Student8@10.8.33.172>
2017-04-04 12:45:00 -0400

    fixed statename

--- Inheritance/src/inheritance/TraceyKong.java
+++ Inheritance/src/inheritance/TraceyKong.java
@@ -5,7 +5,7 @@ public class TraceyKong extends ClubMember {
 	public TraceyKong() {
 		super("Tracey", "Kong", 2017, Main.COMP_SCI_MAJOR, "k-drama");
 	}
-	  
+	   
 	public void act(){
 		System.out.println("My name is Tracey Kong.");
 	}
