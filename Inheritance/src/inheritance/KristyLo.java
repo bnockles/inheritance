99d3575c03be3b230f25c401461d53189790fa85
Student6 <Student6@10.8.33.166>
2017-04-04 12:08:20 -0400

    fo

---
+++ Inheritance/src/inheritance/KristyLo.java
@@ -0,0 +1,12 @@
+package inheritance;
+
+public class KristyLo extends ClubMember {
+
+	public KristyLo() {
+		super("Kristy", "Lo", 2017, Main.COMP_SCI_MAJOR, "disney");
+	}
+	
+	public void act() {
+		super.act();
+	}
+}
