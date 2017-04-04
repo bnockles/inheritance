933b971145119fcba016cde609614d4bbbd38f5f
Student 6 <Student 6@10.8.33.104>
2017-04-04 12:05:15 -0400

    kay

---
+++ Inheritance/src/inheritance/JonathanZheng.java
@@ -0,0 +1,14 @@
+package inheritance;
+
+public class JonathanZheng extends Student {
+
+	public JonathanZheng() {
+		super("Jonathan", "Zheng", 2017, Main.COMP_SCI_MAJOR);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void act(){
+		stateName();
+	}
+
+}
