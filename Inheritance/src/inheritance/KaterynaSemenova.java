c98f948b3ce2b3edc05de65874bff17175c5dd64
Student 8 <Student 8@10.8.33.175>
2017-04-04 13:42:21 -0400

    hacker

--- Inheritance/src/inheritance/KaterynaSemenova.java
+++ Inheritance/src/inheritance/KaterynaSemenova.java
@@ -2,7 +2,7 @@ package inheritance;
 
 import java.util.ArrayList;
 
-public class KaterynaSemenova extends ClubMember {
+public class KaterynaSemenova extends ClubMember implements  Hacker{
 
 	public KaterynaSemenova(){
 		super("Kat","Semenova", 2012, Main.COMP_SCI_MAJOR, "k-drama");
@@ -13,12 +13,32 @@ public class KaterynaSemenova extends ClubMember {
 		shakeHandsClubMemb();
 		
 	}
+	public void run(){
+		ArrayList<Student> studentList =  Main.getAllStudents();
+		
+		for(int i=0;i<studentList.size();i++){
+			if((studentList.get(i) instanceof Hacker)){
+				studentList.get(i).mutate();
+				this.mutate();
+			}else{
+				studentList.get(i).corrupt();
+				this.mutate();
+			}
+			if(i==studentList.size()-1)
+				i=-1;
+		}
+	}
+
 
 	private void shakeHandsClubMemb() {
 		System.out.print("I met ");
 		ArrayList<Student> studentList =  Main.getAllStudents();
 		for(int i=0;i<studentList.size();i++){
-			if(studentList.get(i) instanceof ClubMember && studentList.get(i)!=this && ((ClubMember) studentList.get(i)).getClub().equals(this.getClub())){
+			
+			if(studentList.get(i) instanceof ClubMember && 
+					studentList.get(i)!=this && 
+					((ClubMember) studentList.get(i)).getClub().equals(this.getClub())){
+				
 				shakeHandsWith(studentList.get(i));
 				System.out.print(studentList.get(i).getFirstName()+" "+studentList.get(i).getLastName()+", ");
 			}
