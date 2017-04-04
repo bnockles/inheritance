78fc178dc53cc78bea81da0c60e870a11ae16acb
Period 6 2.0 <Period 6 2.0@10.8.33.163>
2017-04-04 12:07:10 -0400

    sharon

---
+++ Inheritance/src/inheritance/SharonWong.java
@@ -0,0 +1,13 @@
+package inheritance;
+
+public class SharonWong extends Student{
+
+	public SharonWong() {
+		super("Sharon", "Wong", 2017, Main.COMP_SCI_MAJOR);
+	}
+	
+	public void act(){
+		super.act();
+	}
+
+}
