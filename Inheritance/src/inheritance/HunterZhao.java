d1bad40b7e702281e1bfa41a38d3cb61edd4f6ff
student 6 <student 6@10.8.33.151>
2017-04-04 12:05:53 -0400

    started

---
+++ Inheritance/src/inheritance/HunterZhao.java
@@ -0,0 +1,12 @@
+package inheritance;
+
+public class HunterZhao extends Student {
+
+	public HunterZhao() {
+		super("Hunter", "Zhao", 2017, Main.COMP_SCI_MAJOR);
+	}
+	
+	public void act(){
+		stateName();
+	}
+}
