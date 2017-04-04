d8224f71bb77bd85606ab70866c90ec792d7c458
Student 8 <Student 8@10.8.33.175>
2017-04-04 13:09:34 -0400

    shake hands with club

--- Inheritance/src/inheritance/KaterynaSemenova.java
+++ Inheritance/src/inheritance/KaterynaSemenova.java
@@ -1,12 +1,27 @@
 package inheritance;
 
+import java.util.ArrayList;
+
 public class KaterynaSemenova extends ClubMember {
 
 	public KaterynaSemenova(){
-		super("Kat","Semenova", 2012, Main.COMP_SCI_MAJOR, "music");
+		super("Kat","Semenova", 2012, Main.COMP_SCI_MAJOR, "k-drama");
 	}
 	
 	public void act(){
-		System.out.println("Hi, I'm " + this.getFirstName()+" "+this.getLastName());
+		System.out.print("Hi, I'm " + this.getFirstName()+" "+this.getLastName()+". ");
+		shakeHandsClubMemb();
+		
+	}
+
+	private void shakeHandsClubMemb() {
+		System.out.print("I met ");
+		ArrayList<Student> studentList =  Main.getAllStudents();
+		for(int i=0;i<studentList.size();i++){
+			if(studentList.get(i) instanceof ClubMember && studentList.get(i)!=this && ((ClubMember) studentList.get(i)).getClub().equals(this.getClub())){
+				shakeHandsWith(studentList.get(i));
+				System.out.print(studentList.get(i).getFirstName()+" "+studentList.get(i).getLastName()+", ");
+			}
+		}
 	}
 }
