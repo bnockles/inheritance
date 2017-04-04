6704d317023c3492e79d5f7a6860abc637e05751
Student06 <Student06@10.8.33.101>
2017-04-04 12:04:00 -0400

    created kristyTan class

---
+++ Inheritance/src/inheritance/KristyTan.java
@@ -0,0 +1,13 @@
+package inheritance;
+
+public class KristyTan extends Student {
+
+	public KristyTan() {
+		super("Kristy", "Tan", 2017, Main.COMP_SCI_MAJOR);
+	}
+	
+	 public void act(){
+		 
+	 }
+
+}
