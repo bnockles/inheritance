c0e6b99d33099f6464f3fafdfd2d13b10cee8a17
Student 8 <Student 8@10.8.33.158>
2017-04-04 13:47:19 -0400

    competition

--- Inheritance/src/inheritance/JenniberFranco.java
+++ Inheritance/src/inheritance/JenniberFranco.java
@@ -2,7 +2,7 @@ package inheritance;
 
 import java.util.ArrayList;
 
-public class JenniberFranco extends ClubMember {
+public class JenniberFranco extends ClubMember implements Hacker{
 
 	public JenniberFranco() {
 		super("Jenniber","Franco",2017,Main.COMP_SCI_MAJOR,"k-drama");
@@ -22,6 +22,27 @@ public class JenniberFranco extends ClubMember {
 					((ClubMember) s).getClub().equals(this.getClub())){
 				shakeHandsWith(s);
 				System.out.print(s.getFirstName()+" "+s.getLastName()+", ");
+			}
+		}
+	}
+	
+	public void run(){
+		ArrayList<Student> members = Main.getAllStudents();
+		ArrayList<Student> hackers = new ArrayList<Student>();
+		ArrayList<Student> netAdmin = new ArrayList<Student>();
+		for(int i = 0;i<members.size();i++){
+			Student s = members.get(i);
+			if(s instanceof Hacker){
+				s.mutate();
+				this.mutate();
+				hackers.add(s);
+			}
+			else{
+				s.corrupt();
+				netAdmin.add(s);
+			}
+			if(i==members.size()-1){
+				i--;
 			}
 		}
 	}
