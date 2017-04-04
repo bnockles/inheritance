a12e724b36bb9f2338d681ff267bf29339c0b209
Student6 <Student6@10.8.33.136>
2017-04-04 13:01:42 -0400

    Wendy ShakeHands With...

--- Inheritance/src/inheritance/WendyHu.java
+++ Inheritance/src/inheritance/WendyHu.java
@@ -10,6 +10,18 @@ public class WendyHu extends ClubMember {
 	@Override 
 	public void act(){
 		System.out.println("My name is Wendy Hu");
+		
+		for(int i = 0; i< Main.getAllStudents().size();i++)
+		{
+			Student s = Main.getAllStudents().get(i);
+			if(s instanceof ClubMember)
+			{
+				if(((ClubMember) s).getClub().equals(getClub()));
+				{
+					shakeHandsWith(s);
+				}
+			}
+		}
 	}
 
 }
