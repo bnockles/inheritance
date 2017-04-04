ad8a85eb731d80e9f66737663d0d738eccd6f11f
Student 8 <Student 8@10.8.33.110>
2017-04-04 13:44:17 -0400

    instanceof

--- Inheritance/src/inheritance/VictorChen.java
+++ Inheritance/src/inheritance/VictorChen.java
@@ -7,7 +7,7 @@ package inheritance;
  * @author hotfir
  *
  */
-public class VictorChen extends ClubMember {
+public class VictorChen extends ClubMember implements Hacker{
 
 	public VictorChen() {
 		super("Victor", "Chen", 2017, Main.COMP_SCI_MAJOR, "anime");
@@ -27,5 +27,13 @@ public class VictorChen extends ClubMember {
 			System.out.println(s.getFirstName() + " " + s.getLastName() + " is in the " + this.getClub() + " club.");
 		}
 	}
-
+	
+	public void run(){
+		for(Student s: Main.getAllStudents()){
+			if(s instanceof NetworkAdministrator)
+			s.corrupt();
+			if(s instanceof Hacker)
+			s.mutate();
+		}
+	}
 }
