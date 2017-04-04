090536bcd77bcafbd954ff15daae248dbe52cb1f
Student7 <Student7@10.8.33.144>
2017-04-04 12:10:15 -0400

    add afsana Chadni

---
+++ Inheritance/src/inheritance/AfsanaChadni.java
@@ -0,0 +1,13 @@
+package inheritance;
+
+public class AfsanaChadni extends Student {
+
+	public AfsanaChadni() {
+		super("Afsana", "Chadni", 2017, Main.COMP_SCI_MAJOR);		
+	}
+	
+	public void act(){
+		System.out.print("Afsana Chadni");
+	}
+
+}
