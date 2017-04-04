0ed0fa72af3830dae63b3bcca227d6da59fa5a0f
Student 6 <Student 6@10.8.33.173>
2017-04-04 12:03:57 -0400

    Hap Hap

---
+++ Inheritance/src/inheritance/DanielQuinde.java
@@ -0,0 +1,13 @@
+package inheritance;
+
+public class DanielQuinde extends Student {
+	public DanielQuinde(){
+		super("Daniel", "Quinde", 2017, Main.COMP_SCI_MAJOR);
+
+	}
+	
+	public void act(){
+		System.out.println("hi");
+	}
+
+}
