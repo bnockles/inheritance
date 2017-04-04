d051a878b7c138f3a2170e634c16d697be901234
Period 6 2.0 <Period 6 2.0@10.8.33.163>
2017-04-04 12:23:48 -0400

    asdf

--- Inheritance/src/inheritance/SharonWong.java
+++ Inheritance/src/inheritance/SharonWong.java
@@ -3,7 +3,7 @@ package inheritance;
 public class SharonWong extends ClubMember{
 
 	public SharonWong() {
-		super("Sharon", "Wong", 2017, Main.COMP_SCI_MAJOR,"fandom");
+		super("Sharon", "Wong", 2017, Main.COMP_SCI_MAJOR,"pokemon");
 	}
 	
 	public void act(){
