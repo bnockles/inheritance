ffae0ce4aa343d62364ce2e5988562fd492c1b0c
Student6 <Student6@10.8.33.172>
2017-04-04 12:21:01 -0400

    sdfd

--- Inheritance/src/inheritance/SammyajitKapuria.java
+++ Inheritance/src/inheritance/SammyajitKapuria.java
@@ -1,13 +1,13 @@
 package inheritance;
 
-public class SammyajitKapuria extends Student {
+public class SammyajitKapuria extends ClubMember {
 
 	public SammyajitKapuria() {
-		super("Sammy", "Kapuria",2017,Main.COMP_SCI_MAJOR);
+		super("Sammy", "Kapuria",2017,Main.COMP_SCI_MAJOR,"Roblox");
 	}
 	
 	public void act(){ 
-		super.act();
+		//super.act();
 		System.out.println("sAMmy");
 	}
 }
