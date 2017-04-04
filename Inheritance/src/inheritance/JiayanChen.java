904c8110049f958192c58b19431480dcf9b78695
Student 6 <Student 6@10.8.33.156>
2017-04-04 12:07:20 -0400

    hii

--- Inheritance/src/inheritance/JiayanChen.java
+++ Inheritance/src/inheritance/JiayanChen.java
@@ -2,11 +2,8 @@ package inheritance;
 
 public class JiayanChen extends Student {
 
+	
 	public JiayanChen() {
-		// TODO Auto-generated constructor stub
-	}
-
-	public JiayanChen(String name, String surname, int gradClass, Major major) {
 		super("Jiayan", "Chen", 2017, Main.COMP_SCI_MAJOR);
 		// TODO Auto-generated constructor stub
 	}
