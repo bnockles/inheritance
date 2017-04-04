f684369ed466ae2a9d59acab8c7019d56410982b
Student 6 <Student 6@10.8.33.171>
2017-04-04 12:04:05 -0400

    fdas

---
+++ Inheritance/src/inheritance/RayChen.java
@@ -0,0 +1,11 @@
+package inheritance;
+
+public class RayChen extends Student {
+
+	public RayChen(String name, String surname, int gradClass, Major major) {
+		super("Ray", "Chen", 2017, Main.COMP_SCI_MAJOR);
+	}
+	public void act(){
+		System.out.println("insert text here");
+	}
+}
