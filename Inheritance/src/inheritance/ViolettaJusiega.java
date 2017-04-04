f0f630fbe246f3d21ccc83afe3c514f52390e536
Student8 <Student8@10.8.33.154>
2017-04-04 13:09:49 -0400

    v

--- Inheritance/src/inheritance/ViolettaJusiega.java
+++ Inheritance/src/inheritance/ViolettaJusiega.java
@@ -1,5 +1,7 @@
 package inheritance;
 
+import java.util.ArrayList;
+
 public class ViolettaJusiega extends ClubMember{
 	
 	public ViolettaJusiega(){
@@ -8,7 +10,21 @@ public class ViolettaJusiega extends ClubMember{
 	
 	@Override
 	public void act(){
-		System.out.println("Violetta Jusiega");
+		System.out.println("My name is Violetta Jusiega...");
+		
+		ArrayList<Student> studs = Main.getAllStudents();
+		ArrayList<ClubMember> clubStuds = new ArrayList<ClubMember>();
+		for(Student s: studs){
+			ClubMember a = (ClubMember)(s);
+			clubStuds.add(a);
+		}
+		for(ClubMember s: clubStuds){
+			if(s.getClub() == this.getClub()){
+				shakeHandsWith(s);
+			}
+		}
+		
 	}
+	
 	
 }
