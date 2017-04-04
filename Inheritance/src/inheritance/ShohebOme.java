9afe73bd337afa526b7bbc9308936c4b747d08a9
Student6 <Student6@10.8.33.154>
2017-04-04 12:20:56 -0400

    hehe xd

--- Inheritance/src/inheritance/ShohebOme.java
+++ Inheritance/src/inheritance/ShohebOme.java
@@ -1,13 +1,13 @@
 package inheritance;
 
-public class ShohebOme extends Student {
+public class ShohebOme extends ClubMember {
 
 	public ShohebOme() {
-		super("Shoheb", "Ome", 2017, Main.COMP_SCI_MAJOR);
+		super("Shoheb", "Ome", 2017, Main.COMP_SCI_MAJOR, "Roblox");
 	}
 	
 	public void act(){
-		System.out.println("Shoheb Ome reporting for duty! XDDD");
+		System.out.println("Shoheb Ome reporting for duty! XDDD\n\n\n\n\n");
 	}
 	//loll
 }
