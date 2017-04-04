3d5de6badd3d3d6c778bcb556afc59b9ed183146
Student 6 <Student 6@10.8.33.110>
2017-04-04 12:04:43 -0400

    Alex

---
+++ Inheritance/src/inheritance/AlexHuang.java
@@ -0,0 +1,11 @@
+package inheritance;
+
+public class AlexHuang extends Student{
+
+	public AlexHuang() {
+		super("Alex","Huang",2017,Main.COMP_SCI_MAJOR);
+	}
+	public void act(){
+		System.out.println("Alex Huang");
+	}
+}
