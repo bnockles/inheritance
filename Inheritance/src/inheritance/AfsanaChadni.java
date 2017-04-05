f0acce4eb355c4c2745a681c85c7a06942fcb4a6
Student7 <Student7@10.8.33.144>
2017-04-05 12:22:18 -0400

    Handshake

--- Inheritance/src/inheritance/AfsanaChadni.java
+++ Inheritance/src/inheritance/AfsanaChadni.java
@@ -3,20 +3,17 @@ package inheritance;
 public class AfsanaChadni extends ClubMember {
 
 	public AfsanaChadni() {
-		super("Afsana", "Chadni", 2017, Main.COMP_SCI_MAJOR, "pokemon");		
+		super("Afsana", "Chadni", 2017, Main.COMP_SCI_MAJOR, "pokemon");
 	}
-	
-	public void act(){
+
+	public void act() {
 		System.out.print("Afsana Chadni");
-		for(int i = 0; i < Main.getAllStudents().size(); i++){
-			if (Main.getAllStudents().get(i) instanceof ClubMember 
-					&& ((ClubMember) (Main.getAllStudents().get(i))).getClub().equals("pokemon") 
-					&& Main.getAllStudents().get(i) != this){
-						shakeHandsWith(Main.getAllStudents().get(i));
-					}
+		for (int i = 0; i < Main.getAllStudents().size(); i++) {
+			if (Main.getAllStudents().get(i) instanceof ClubMember
+					&& ((ClubMember) (Main.getAllStudents().get(i))).getClub().equals("pokemon")
+					&& Main.getAllStudents().get(i) != this) {
+				shakeHandsWith(Main.getAllStudents().get(i));
+			}
 		}
 	}
-	
-	
-
 }
