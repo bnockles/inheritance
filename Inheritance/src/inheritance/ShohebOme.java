bea82b2dc9a15293d2ab3d9be51384d2381edcca
Student6 <Student6@10.8.33.154>
2017-04-05 12:09:57 -0400

    efkanflk

--- Inheritance/src/inheritance/ShohebOme.java
+++ Inheritance/src/inheritance/ShohebOme.java
@@ -9,7 +9,7 @@ public class ShohebOme extends ClubMember {
 	public void act(){
 		for(Student s: Main.getAllStudents()){
 			ClubMember c = (ClubMember) s;
-			if(c.getClub() == this.getClub()){
+			if(c.getClub().equals(this.getClub())){
 				shakeHandsWith(s);
 			}
 		}
