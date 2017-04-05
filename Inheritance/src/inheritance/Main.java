e23290e5993e5933fcc0057814a8d4dcfb350f98
Nockles <Nockles@10.8.33.96>
2017-04-05 12:14:02 -0400

    added message game

--- Inheritance/src/inheritance/Main.java
+++ Inheritance/src/inheritance/Main.java
@@ -13,114 +13,172 @@ public class Main {
 
 	private final static ArrayList<Student> students= new ArrayList<Student>();
 	public static final Major COMP_SCI_MAJOR = new Major("Computer Science","AP Java");
-	
-	
+
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-//		students.add(new IllhamAlam());
-//		students.add(new AfsanaChadni());
-//		students.add(new JiayanChen());
-//		students.add(new RayChen());
-//		students.add(new MatthewChu());
-//		students.add(new AliEldeeb());
-//		students.add(new DanielGoon());
-//		students.add(new AnnaHo());
-//		students.add(new AlexHuang());
-//		students.add(new SammyajitKapuria());
-//		students.add(new JasonLau());
-//		students.add(new VickiLau());
-//		students.add(new MichaelLi());
-//		students.add(new KristyLo());
-//		students.add(new BillyLuy());
-//		students.add(new EdwinMa());
-//		students.add(new JiaMingMa());
-//		students.add(new DavidMedina());
-//		students.add(new HansMontero());
-//		students.add(new MatthewNguyen());
-//		students.add(new ShohebOme());
-//		students.add(new DanielQuinde());
-////		students.add(new MakinoonSami());
-////		students.add(new IramShahed());
-//		students.add(new RyanSitu());
-//		students.add(new NikitaSmirnov());
-//		students.add(new DavidSzymanski());
-//		students.add(new KristyTan());
-//		students.add(new SharonWong());
-//		students.add(new MatthewYarmolinsky());
-//		students.add(new AriqZaman());
-//		students.add(new HunterZhao());
-//		students.add(new JonathanZheng());
-//		students.add(new KevinZheng());
+//				students.add(new IllhamAlam());
+				students.add(new AfsanaChadni());
+				students.add(new JiayanChen());
+				students.add(new RayChen());
+				students.add(new MatthewChu());
+				students.add(new AliEldeeb());
+				students.add(new DanielGoon());
+				students.add(new AnnaHo());
+				students.add(new AlexHuang());
+				students.add(new SammyajitKapuria());
+				students.add(new JasonLau());
+				students.add(new VickiLau());
+				students.add(new MichaelLi());
+				students.add(new KristyLo());
+				students.add(new BillyLuy());
+				students.add(new EdwinMa());
+				students.add(new JiaMingMa());
+				students.add(new DavidMedina());
+				students.add(new HansMontero());
+				students.add(new MatthewNguyen());
+				students.add(new ShohebOme());
+				students.add(new DanielQuinde());
+		//		students.add(new MakinoonSami());
+		//		students.add(new IramShahed());
+				students.add(new RyanSitu());
+				students.add(new NikitaSmirnov());
+				students.add(new DavidSzymanski());
+				students.add(new KristyTan());
+				students.add(new SharonWong());
+				students.add(new MatthewYarmolinsky());
+				students.add(new AriqZaman());
+				students.add(new HunterZhao());
+				students.add(new JonathanZheng());
+				students.add(new KevinZheng());
 
-//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!PERIOD 8
+		//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!PERIOD 8
 
-		students.add(new VictorChen());
-		students.add(new KatherineCheng());
-		students.add(new AnthonyCuzzi());
-		students.add(new JoyceFeng());
-		students.add(new JenniberFranco());
-		students.add(new MaxFriedman());
-		students.add(new GabrielHo());
-		students.add(new WendyHu());
-		students.add(new TobyHuang());
-		students.add(new JosephHur());
-//		students.add(new SyedHussain());
-		students.add(new TamannaHussain());
-		students.add(new VeerajJethalal());
-		students.add(new ViolettaJusiega());
-		students.add(new SimonKahn());
-		students.add(new AhmedKhan());
-		students.add(new MahinKhan());
-		students.add(new TraceyKong());
-		students.add(new JoeyLeung());
-		students.add(new FultonLin());
-		students.add(new ZhengLu());
-		students.add(new RichardLy());
-		students.add(new RisaMorishima());
-		students.add(new EduardoRouse());
-		students.add(new SamSagawa());
-		students.add(new KaterynaSemenova());
-		students.add(new JaviyWang());
-//		students.add(new JimmyWu());
-		students.add(new ZhenXiao());
-		students.add(new HaoYuan());
-		students.add(new IvanZhen());
-		students.add(new KevinZhen());
-		students.add(new DanielZubarchuk());
-
-
+//		students.add(new VictorChen());
+//		students.add(new KatherineCheng());
+//		students.add(new AnthonyCuzzi());
+//		students.add(new JoyceFeng());
+//		students.add(new JenniberFranco());
+////		students.add(new MaxFriedman());
+//		students.add(new GabrielHo());
+//		students.add(new WendyHu());
+//		students.add(new TobyHuang());
+//		students.add(new JosephHur());
+//		//		students.add(new SyedHussain());
+//		students.add(new TamannaHussain());
+//		students.add(new VeerajJethalal());
+//		students.add(new ViolettaJusiega());
+//		students.add(new SimonKahn());
+//		students.add(new AhmedKhan());
+//		students.add(new MahinKhan());
+//		students.add(new TraceyKong());
+//		students.add(new JoeyLeung());
+//		students.add(new FultonLin());
+//		students.add(new ZhengLu());
+//		students.add(new RichardLy());
+//		students.add(new RisaMorishima());
+//		students.add(new EduardoRouse());
+//		students.add(new SamSagawa());
+//		students.add(new KaterynaSemenova());
+//		students.add(new JaviyWang());
+//		//		students.add(new JimmyWu());
+////		students.add(new ZhenXiao());
+//		students.add(new HaoYuan());
+//		students.add(new IvanZhen());
+//		students.add(new KevinZhen());
+//		students.add(new DanielZubarchuk());
+//
+//
 		rollCall(students);
 		System.out.println("");
 		checkCompliance();
+//		play();
+//		Message message= new Message(159819125);
+//		message.start(students.get((int)(students.size()*Math.random())));
 	}
-	
+
 	public final static ArrayList<Student> getAllStudents(){
 		return students;
 	}
 
 	private final static void play(){
-		
+
+		//creates the Threads
 		for(Student s: students){
 			Thread t = new Thread(s);
 			s.setPlay(t);
-			t.start();
+			System.out.println(s.getFirstName()+" has entered the game.");
+		}
+		//starts the Threads
+		for(Student s: students){		
+			s.getPlay().start();
 		}
 		Thread checkForDead = new Thread(new Runnable() {
-			
+
 			@Override
 			public void run() {
-				for(Student s: students){
-					if(!s.isAlive()){
-						s.getPlay().interrupt();
+				A: while(true){
+					for(int i = 0; i < students.size(); i++){
+						Student s = students.get(i);
+						if(!s.isAlive() && !s.isReportedDead()){
+							System.out.println(s.getFirstName()+" is dead!");
+							s.getPlay().interrupt();
+							s.setReportedDead(true);
+						}
+					}
+					int hackerCount = 0;
+					int networkAdminCount = 0;
+							
+					for(int i = 0; i < students.size(); i++){
+						Student s = students.get(i);
+						if(s.isAlive() && s instanceof Hacker){
+							hackerCount++;
+						}else if(s.isAlive() && s instanceof NetworkAdministrator){
+							networkAdminCount++;
+						}	
+					}
+					System.out.println("Remaining Hackers: "+hackerCount+", remaining N.A.s: "+networkAdminCount);
+					if(hackerCount == 0){
+						System.out.println("Network Admins Win!");
+						break A;
+					}else if(networkAdminCount == 0){
+						System.out.println("Hackers Win!");
+						break A;
 					}
 				}
+//				Student s = students.get(0);
+//				System.out.println(s.getFirstName()+" is the winner!");
+//				s.getPlay().interrupt();
 			}
 		});
 		checkForDead.start();
+
+	}
+
+	public static class Message{
+		
+		private long serial;
+		private ArrayList<Student> recordOfAllBearers; 
+		
+		private Message(long id){
+			this.serial = id;
+			recordOfAllBearers = new ArrayList<Student>();
+		}
+		
+		private void start(Student start){
+			recordOfAllBearers.add(start);
+		}
+		
+		public void pass(Student from, Student to){
+			
+		}
+		
 		
 	}
+	
+	
 	
 	public static void rollCall(ArrayList<Student> students) {
 		for(Student s: students){
@@ -134,51 +192,51 @@ public class Main {
 	}
 
 	public static void checkCompliance(){
-	for(Student s: students){
-	if(s instanceof ClubMember){
-	ClubMember cm = (ClubMember)s;
-	int shakenHands = 0;
-	boolean onlyMembers = true;
-	
-	for(Student met: s.getHandshakes()){
-	if(met instanceof ClubMember){
-	ClubMember cmShaken = (ClubMember)met;
-	if(cmShaken.getClub().equals(cm.getClub())){
-	shakenHands++;
-	}else{
-	System.out.println(s.getFirstName() +" "+s.getLastName()+" shook the hand of a person who is in a different club!.");
-	onlyMembers = false;
-	break;
-	}
-	}else{
-	System.out.println(s.getFirstName() +" "+s.getLastName()+" shook the hand of a person who is not in any club!.");
-	onlyMembers = false;
-	break;
-	}
-	}
-	int numberOfClubMembers = countMembers(cm.getClub());
-	if(onlyMembers && shakenHands == numberOfClubMembers -1){
-	System.out.println("!!!!!!!! "+s.getFirstName() +" "+s.getLastName()+" shook the hands of everyone else in the club!!!!!!!!");
-	}else if(onlyMembers && shakenHands == numberOfClubMembers){
-	System.out.println(""+s.getFirstName() +" "+s.getLastName()+" shook the hands of everyone else in the club, including his- or herself!");
-	}else if (onlyMembers){
-	System.out.println(""+s.getFirstName() +" "+s.getLastName()+" shook the hands of some (but not all) of the people in the club.");
-	}
-	}else{
-	System.out.println(s.getFirstName() +" "+s.getLastName()+" is not a member of a club.");
-	}
-	}
-	}
-	
-	private static int countMembers(String club) {
-	int count = 0;
-	for(Student s: students){
-	if(s instanceof ClubMember){
-	ClubMember cm = (ClubMember)s;
-	if(cm.getClub().equals(club))count++;
-	}
-	}
-	return count;
+		for(Student s: students){
+			if(s instanceof ClubMember){
+				ClubMember cm = (ClubMember)s;
+				int shakenHands = 0;
+				boolean onlyMembers = true;
+
+				for(Student met: s.getHandshakes()){
+					if(met instanceof ClubMember){
+						ClubMember cmShaken = (ClubMember)met;
+						if(cmShaken.getClub().equals(cm.getClub())){
+							shakenHands++;
+						}else{
+							System.out.println(s.getFirstName() +" "+s.getLastName()+" shook the hand of a person who is in a different club!.");
+							onlyMembers = false;
+							break;
+						}
+					}else{
+						System.out.println(s.getFirstName() +" "+s.getLastName()+" shook the hand of a person who is not in any club!.");
+						onlyMembers = false;
+						break;
+					}
+				}
+				int numberOfClubMembers = countMembers(cm.getClub());
+				if(onlyMembers && shakenHands == numberOfClubMembers -1){
+					System.out.println("!!!!!!!! "+s.getFirstName() +" "+s.getLastName()+" shook the hands of everyone else in the club!!!!!!!!");
+				}else if(onlyMembers && shakenHands == numberOfClubMembers){
+					System.out.println(""+s.getFirstName() +" "+s.getLastName()+" shook the hands of everyone else in the club, including his- or herself!");
+				}else if (onlyMembers){
+					System.out.println(""+s.getFirstName() +" "+s.getLastName()+" shook the hands of some (but not all) of the people in the club.");
+				}
+			}else{
+				System.out.println(s.getFirstName() +" "+s.getLastName()+" is not a member of a club.");
+			}
+		}
 	}
 
+	private static int countMembers(String club) {
+		int count = 0;
+		for(Student s: students){
+			if(s instanceof ClubMember){
+				ClubMember cm = (ClubMember)s;
+				if(cm.getClub().equals(club))count++;
+			}
+		}
+		return count;
+	}
+	
 }
