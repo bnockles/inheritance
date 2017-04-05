9bbb3dd3d346f539ed46387345fc5a3380e3c643
Student6 <Student6@10.8.33.172>
2017-04-05 12:14:56 -0400

    asdsad

--- Inheritance/src/inheritance/SammyajitKapuria.java
+++ Inheritance/src/inheritance/SammyajitKapuria.java
@@ -1,14 +1,25 @@
 package inheritance;
 
+import java.util.ArrayList;
+
 public class SammyajitKapuria extends ClubMember {
 
 	public SammyajitKapuria() {
-		super("Sammy", "Kapuria",2017,Main.COMP_SCI_MAJOR,"Roblox");
+		super("Sammy", "Kapuria",2017,Main.COMP_SCI_MAJOR,"roblox");
 	}
 	
 	public void act(){ 
 		//super.act();
 		System.out.println("sAMmy");
+		ArrayList<Student> allStudents = Main.getAllStudents();
+		for(Student s: allStudents){
+			if(s instanceof ClubMember && ((ClubMember) s).getClub()=="roblox"){
+				shakeHandsWith(s);
+				System.out.println("I shook hands with "+s+ " !");
+			}
+		}
+		
 	}
 }
+
 
