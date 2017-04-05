97cd0f5f65b795d7f71429be6eb974bab8a0c077
simonblast0 <swk1124@gmail.com>
2017-04-05 14:03:20 -0400

    this

--- Inheritance/src/inheritance/SimonKahn.java
+++ Inheritance/src/inheritance/SimonKahn.java
@@ -1,6 +1,10 @@
 package inheritance;
 
-public class SimonKahn extends ClubMember implements Hacker{
+import java.time.Instant;
+
+import inheritance.Main.Message;
+
+public class SimonKahn extends ClubMember implements Spy{
 
 	public SimonKahn() {
 		super("Simon", "Kahn", 666, Main.COMP_SCI_MAJOR, "tcg");
@@ -13,10 +17,10 @@ public void act(){
 	run();
 }
 	public void run(){
-		for(Student i: Main.getAllStudents()){
-		if (i.getFirstName()!="Simon"){
-			i.corrupt();
-		}
+//		for(Student i: Main.getAllStudents()){
+//		if (i.getFirstName()!="Simon"){
+//			i.corrupt();
+//		}
 		//if (((ClubMember) i).getClub().equals("tcg")){
 			//shakeHandsWith(i);
 			
@@ -25,5 +29,14 @@ public void act(){
 		//for(Student i: getHandshakes()){
 		//System.out.print(" "+i.getFirstName());
 	}
-	}
+	
+public void recievedMessage(Message message)
+{
+	for(int i=0; i <Main.getAllStudents().size()-1;i++){
+		if(Main.getAllStudents().get(i) instanceof Soviet){
+Message.pass(Main.getAllStudents().get(i+1), Main.getAllStudents().get(0));
+		}
+		}
+}
+
 }
