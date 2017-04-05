e320478d527589811afed099d9a5a04c882f2302
Student 6 <Student 6@10.8.33.175>
2017-04-05 12:46:48 -0400

    pls work

--- Inheritance/src/inheritance/DanielGoon.java
+++ Inheritance/src/inheritance/DanielGoon.java
@@ -7,23 +7,28 @@ public class DanielGoon extends ClubMember implements Hacker{
 	private ArrayList<NetworkAdministrator> nwdm;
 	public DanielGoon(){
 		super("Daniel","Goon",2017,Main.COMP_SCI_MAJOR,"minecraft");
+//		for(Student s: Main.getAllStudents()){
+//			if(s instanceof Hacker){
+//				fourChan.add((Hacker)s);
+//			}
+//		}
+//		for(Student s: Main.getAllStudents()){
+//			if(s instanceof NetworkAdministrator){
+//				nwdm.add((NetworkAdministrator)s);
+//			}
+//		}
+	}
+	public void act(){
+//		System.out.println(getFirstName() +" "+ getLastName());
+//		for(Student s: Main.getAllStudents()){
+//			if(s instanceof ClubMember && ((ClubMember) s).getClub().equals(getClub()) 
+//					&& s != this){
+//				shakeHandsWith(s);
+//			}
+//		}
 		for(Student s: Main.getAllStudents()){
 			if(s instanceof Hacker){
 				fourChan.add((Hacker)s);
-			}
-		}
-		for(Student s: Main.getAllStudents()){
-			if(s instanceof NetworkAdministrator){
-				nwdm.add((NetworkAdministrator)s);
-			}
-		}
-	}
-	public void act(){
-		System.out.println(getFirstName() +" "+ getLastName());
-		for(Student s: Main.getAllStudents()){
-			if(s instanceof ClubMember && ((ClubMember) s).getClub().equals(getClub()) 
-					&& s != this){
-				shakeHandsWith(s);
 			}
 		}
 	}
