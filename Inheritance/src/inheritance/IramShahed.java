586590708c3682c555f2e40cca87d67d88f53a65
Student 8 <Student 8@10.8.33.155>
2017-04-05 12:02:39 -0400

    dasdasdsad

---
+++ Inheritance/src/inheritance/IramShahed.java
@@ -0,0 +1,11 @@
+package inheritance;
+
+public class IramShahed extends ClubMember {
+
+	public IramShahed() {
+		super("Iram", "Shahed", 2017, Main.COMP_SCI_MAJOR, "runescape");
+	}
+	public void act(){
+		System.out.println("TROLOLOLOLOLLLLLOOOOOLLLLOOOOOOLLLLLOOOLahahhahhahahahahjhahahahah");
+	}
+}
