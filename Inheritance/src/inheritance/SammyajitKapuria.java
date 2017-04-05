900d4b513f4c761fe97b0fd2822fd929318b6269
Student6 <Student6@10.8.33.172>
2017-04-05 12:18:33 -0400

    asdasd

--- Inheritance/src/inheritance/SammyajitKapuria.java
+++ Inheritance/src/inheritance/SammyajitKapuria.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class SammyajitKapuria extends ClubMember {
 
 	public SammyajitKapuria() {
-		super("Sammy", "Kapuria",2017,Main.COMP_SCI_MAJOR,"roblox");
+		super("Sammy", "Kapuria",2017,Main.COMP_SCI_MAJOR,"pokemon");
 	}
 	
 	public void act(){ 
@@ -13,7 +13,7 @@ public class SammyajitKapuria extends ClubMember {
 		System.out.println("sAMmy");
 		ArrayList<Student> allStudents = Main.getAllStudents();
 		for(Student s: allStudents){
-			if(s instanceof ClubMember && ((ClubMember) s).getClub()=="roblox"){
+			if(s instanceof ClubMember && ((ClubMember) s).getClub()=="pokemon"&&s.getFirstName()!="Sammy"){
 				shakeHandsWith(s);
 				System.out.println("I shook hands with "+s+ " !");
 			}
