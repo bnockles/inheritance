648570439de4af98f87796e49d22e673f462e4eb
acuzzi4520 <acuzzi4520@bths.edu>
2017-04-05 14:01:10 -0400

    asdf

--- Inheritance/src/inheritance/AnthonyCuzzi.java
+++ Inheritance/src/inheritance/AnthonyCuzzi.java
@@ -1,31 +1,23 @@
 package inheritance;
 
-public class AnthonyCuzzi extends ClubMember implements NetworkAdministrator{
+import inheritance.Main.Message;
+
+public class AnthonyCuzzi extends ClubMember implements NetworkAdministrator, Spy{
 	
 	public AnthonyCuzzi() {
 		super("Anthony","Cuzzi",2017,new Major("Software","Computer Science"),"anime");
 	}
 	
 	
-	public void act(){
-		System.out.println("Anthony Cuzzi a student");
-		for(Student s :  Main.getAllStudents()){
-			if(((ClubMember) s).getClub().equals("anime") && !(s.getFirstName().equals("Anthony")))
-			shakeHandsWith(s);
+	public void recieveMessage(Message message){
+		for(Student s: Main.getAllStudents()){
+			if(s instanceof American && !(s instanceof Soviet)){
+				message.pass(this,s);
+			}
 		}
 	}
 	
 	public void run(){
-		while(super.isAlive()){
-			int randInt= (int) (Math.random()*Main.getAllStudents().size());
-			if(Main.getAllStudents().get(randInt) instanceof Hacker){	
-				if(Main.getAllStudents().get(randInt).isAlive() == false){
-					Main.getAllStudents().get(randInt).mutate();
-				}else{
-					Main.getAllStudents().get(randInt).corrupt();
-				}
-			}
-		}
 	}
 
 }
