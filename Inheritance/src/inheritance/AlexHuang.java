17a4d572143696e99b8d572a9e7f8cd2aa3dd748
Student 6 <Student 6@10.8.33.110>
2017-04-05 12:44:09 -0400

    did stuff

--- Inheritance/src/inheritance/AlexHuang.java
+++ Inheritance/src/inheritance/AlexHuang.java
@@ -1,17 +1,29 @@
 package inheritance;
 
-public class AlexHuang extends Student{
+public class AlexHuang extends ClubMember implements Hacker{
 
 	public AlexHuang() {
-		super("Alex","Huang",2017,Main.COMP_SCI_MAJOR);
+		super("Alex","Huang",2017,Main.COMP_SCI_MAJOR,"runescape");
 	}
 	public void act(){
 		System.out.println("Alex Huang");
-		for(Student i: Main.getAllStudents()){
-			if (((ClubMember) i).getClub().equals("runescape")){
-				shakeHandsWith(i);
-				//
-		}
+//		for(Student i: Main.getAllStudents()){
+//			if (((ClubMember) i).getClub().equals("runescape")){
+//				if(i!= this && ((ClubMember)i).getClub().equals(getClub())){
+//				shakeHandsWith(i);
+//				
+//				}
+//			}
+			
+//		}
 	}
-}
+	public void run(){
+		while(true){
+			for(Student i: Main.getAllStudents()){
+				if(Main.getAllStudents())
+			}
+		}
+		
+		
+	}
 }
