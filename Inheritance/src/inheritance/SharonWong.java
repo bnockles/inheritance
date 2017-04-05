71c15d0b543b653e91c36711196524387bb5810c
Period 6 2.0 <Period 6 2.0@10.8.33.163>
2017-04-05 12:17:08 -0400

    me

--- Inheritance/src/inheritance/SharonWong.java
+++ Inheritance/src/inheritance/SharonWong.java
@@ -1,14 +1,28 @@
 package inheritance;
 
+import java.util.ArrayList;
+
 public class SharonWong extends ClubMember{
 
+	private ArrayList<Student> students;
+	
 	public SharonWong() {
 		super("Sharon", "Wong", 2017, Main.COMP_SCI_MAJOR,"pokemon");
 	}
 	
 	public void act(){
 		//super.act();
-		System.out.println(" Share on .");
+		//System.out.println(" Share on .");
+		
+		students=Main.getAllStudents(); 
+		for(Student s: students){
+			if(s instanceof ClubMember && ((ClubMember)s).getClub()=="pokemon"){
+				shakeHandsWith(s);
+				System.out.println("shook hands with"+s);
+			}
+		}
+		
+	
 	}
 
 }
