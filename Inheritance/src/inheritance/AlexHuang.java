df2a83f0128a7ab5da31c1620e649022da8afe92
Student 6 <Student 6@10.8.33.110>
2017-04-05 12:13:17 -0400

    rr

--- Inheritance/src/inheritance/AlexHuang.java
+++ Inheritance/src/inheritance/AlexHuang.java
@@ -1,16 +1,16 @@
 package inheritance;
 
-public class AlexHuang extends ClubMember{
+public class AlexHuang extends Student{
 
 	public AlexHuang() {
-		super("Alex","Huang",2017,Main.COMP_SCI_MAJOR,"runescape");
+		super("Alex","Huang",2017,Main.COMP_SCI_MAJOR);
 	}
 	public void act(){
 		System.out.println("Alex Huang");
 		for(Student i: Main.getAllStudents()){
 			if (((ClubMember) i).getClub().equals("runescape")){
 				shakeHandsWith(i);
-				
+				//
 		}
 	}
 }
