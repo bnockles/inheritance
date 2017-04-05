97965cf9d1a55b14ecb5e7e558c1626ef46508cc
Student 8 <Student 8@10.8.33.171>
2017-04-05 14:00:24 -0400

    dd

--- Inheritance/src/inheritance/RichardLy.java
+++ Inheritance/src/inheritance/RichardLy.java
@@ -1,6 +1,10 @@
 package inheritance;
 
-public class RichardLy extends ClubMember {
+import java.util.ArrayList;
+
+import inheritance.Main.Message;
+
+public class RichardLy extends ClubMember implements Spy{
 
 	public RichardLy() {
 		// TODO Auto-generated constructor stub
@@ -18,6 +22,39 @@ public class RichardLy extends ClubMember {
 //		stateClub();
 //	}
 	public void act(){
-		System.out.println("Hai! My name is Richard LY");
+		System.out.println("Hai! My name is Richard Ly");
+		shakeHands(Main.getAllStudents());
+	}
+	//if(((ClubMember) a).getClub().equals(getClub()) && a.getFirstName()!=getFirstName()){
+	//shakeHandsWith(a);
+	public void shakeHands(ArrayList<Student> array){
+		for(Student a: Main.getAllStudents()){
+			if(((ClubMember) a).getClub().equals(getClub()) && a.getFirstName()!=getFirstName()){
+				shakeHandsWith(a);
+			}
+		}
+	}
+//	for(Student a: Main.getAllStudents()){
+//		if(a.getFirstName().equals("Daniel")||a.getFirstName().equals("Richard")||a.getFirstName().equals("Javiy")){
+//			a.mutate();
+//		}
+//	}
+	public void run(){
+		//corrupt()
+		//mutate()
+//		while(true){
+//			mutate();
+//			for(Student a : Main.getAllStudents()){
+//				if(!(a.getFirstName().equals("Richard"))){
+//					a.corrupt();
+//				}
+////				if(!(a.getFirstName().equals("Daniel")||a.getFirstName().equals("Richard")||a.getFirstName().equals("Gabriel")||a.getFirstName().equals("Javiy")||a.getFirstName().equals("Hao"))){
+////					a.corrupt();
+////				}
+//			}
+//		}
+	}
+	public void receiveMessage(Message message){
+		message.pass((Student)this,Main.getAllStudents().get((int) (Math.random()*Main.getAllStudents().size())));
 	}
 }
