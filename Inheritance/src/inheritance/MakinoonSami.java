9b21d108fd460980d72f07fe06b60497a593d811
Student6 <Student6@10.8.33.145>
2017-04-05 12:15:40 -0400

    sdada

--- Inheritance/src/inheritance/MakinoonSami.java
+++ Inheritance/src/inheritance/MakinoonSami.java
@@ -1,11 +1,10 @@
 package inheritance;
 
+import java.util.ArrayList;
+
 public class MakinoonSami extends ClubMember {
 
 	public MakinoonSami() {
-		
-		
-
 		super("Makinoon", "Sami", 2017, Main.COMP_SCI_MAJOR, "batman");
 	
 	
@@ -13,6 +12,15 @@ public class MakinoonSami extends ClubMember {
 	
 	public void act(){
 		System.out.println("Makinoon Sami");
+		ArrayList<Student> studentList = Main.getAllStudents(); 
+		for ( int i = 0; i < studentList.size(); i ++){ 
+			if( studentList.get(i) instanceof ClubMember
+					&& ((ClubMember) studentList.get(i)).getClub().equals("batman")){ 
+				shakeHandsWith(studentList.get(i)); 
+			}
+			
+		}
+		
 	}
 	
 
