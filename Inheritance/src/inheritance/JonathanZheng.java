9dc4acef1f49d03fde1ca6461bb84834cd6f80ac
Student 6 <Student 6@10.8.33.104>
2017-04-05 12:18:36 -0400

    aiya

--- Inheritance/src/inheritance/JonathanZheng.java
+++ Inheritance/src/inheritance/JonathanZheng.java
@@ -3,7 +3,7 @@ package inheritance;
 import java.util.ArrayList;
 
 public class JonathanZheng extends ClubMember {
-
+//
 	public JonathanZheng() {
 		super("Jonathan", "Zheng", 2017, Main.COMP_SCI_MAJOR, "batman");
 		// TODO Auto-generated constructor stub
