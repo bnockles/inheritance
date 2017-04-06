c1a4c4a613d9c2c135bf9b897e11cb47523040d3
Student 6 <Student 6@10.8.33.171>
2017-04-06 12:19:18 -0400

    asdf

--- Inheritance/src/inheritance/RayChen.java
+++ Inheritance/src/inheritance/RayChen.java
@@ -34,8 +34,9 @@ public class RayChen extends ClubMember implements NetworkAdministrator, Soviet{
 	
 	public void receiveMessage(Message m){
 		ArrayList<Student> students = Main.getAllStudents();
+		ArrayList<Integer> passedTo = new ArrayList<Integer>();
 		int num = (int)(Math.random()*students.size());
-		while(students.get(num) instanceof American || students.get(num).equals(this)){
+		while(passedTo.contains(num) || students.get(num) instanceof American || students.get(num).equals(this)){
 			num = (int)(Math.random()*students.size());
 		}
 		m.pass(this, students.get(num));
