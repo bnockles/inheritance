2c7369d045a8dde6aa8b348ceb5489af6d4f1ed3
Student7 <Student7@10.8.33.144>
2017-04-06 13:08:49 -0400

    asdsda

--- Inheritance/src/inheritance/JimmyWu.java
+++ Inheritance/src/inheritance/JimmyWu.java
@@ -1,49 +1,58 @@
 package inheritance;
 
 import inheritance.Main.Message;
-//
-public class JimmyWu extends ClubMember implements   NetworkAdministrator,Soviet{
+
+public class JimmyWu extends ClubMember implements NetworkAdministrator,Soviet{
+
+	private int nextStudentIndex = 0;
 
 	public JimmyWu() {
 		super("Jimmy", "Wu", 2017, Main.COMP_SCI_MAJOR, "k-pop");
-
 	}
 	
-	
-	@Override
-	public void act(){ 
-		System.out.println("My name is Jimmy");
-		
-		for(int i = 0; i < Main.getAllStudents().size(); i++){
-			Student s = Main.getAllStudents().get(i);
-			if( s instanceof ClubMember && ((ClubMember) s).getClub().equals(this.getClub()) && !s.equals(this)){
-				shakeHandsWith(s);
-			}
-		}
-	} 
-	
-	@Override
 	public void run(){
 		while(true){
 			for(Student s: Main.getAllStudents()){
-				if(s instanceof NetworkAdministrator && !s.isAlive()){
+				if(s instanceof NetworkAdministrator){
 					s.restore();
-				}
-				else if(s instanceof Hacker && s.isAlive()){
+				}else if(s instanceof Hacker){
 					s.createPatch();
 				}
 			}
 		}
 	}
 	
-	@Override
+	public void act(){
+		System.out.println("What's poppin? It's Jimmy Wu.");
+		shakeAllHands();
+	}
+	
+	public void stateStatus() {
+		System.out.print("After I complete "+getMajor().getCulminatingCourse()+", I'll make mad bank.");
+	}
+	
+	public void shakeAllHands(){
+		for(Student c: Main.getAllStudents()){
+			ClubMember s = (ClubMember) c;
+			if(s.getClub().equals(getClub()) && !(s instanceof JimmyWu)){
+				shakeHandsWith(c);
+			}
+		}
+	}
+	
 	public void receiveMessage(Message message){
-		for(Student s: Main.getAllStudents()){
-			if(s instanceof Soviet  && !(s instanceof American)){
-				System.out.println("rush b cyka blyat");
-				message.pass(this, s);
+		for(int i=nextStudentIndex; i<Main.getAllStudents().size(); i++){
+//			if(Main.getAllStudents().get(i) instanceof Soviet && !(Main.getAllStudents().get(i) instanceof Spy)){
+//				nextStudentIndex = i+1;
+//				message.pass(this, Main.getAllStudents().get(i));
+//				System.out.println("Cyka Blyat");
+//				break;
+//			}
+			if(Main.getAllStudents().get(i).getFirstName().equals("Zheng")){
+				message.pass(this, Main.getAllStudents().get(i));
 				break;
 			}
 		}
 	}
+
 }
