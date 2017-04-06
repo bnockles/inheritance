5886f5149825f4a481a3911a348c9cce06f1229c
Student6 <Student6@10.8.33.157>
2017-04-06 12:30:07 -0400

    ygh

--- Inheritance/src/inheritance/JasonLau.java
+++ Inheritance/src/inheritance/JasonLau.java
@@ -7,7 +7,6 @@ import inheritance.Main.Message;
 public class JasonLau extends ClubMember implements Hacker, Soviet {
 	
 	ArrayList<Student> arr = Main.getAllStudents();
-	ArrayList<Student> sovs = new ArrayList<Student>();
 	String message = "ryan pass teh wheat";
 	int b = 0;
 	public JasonLau(){
@@ -24,12 +23,6 @@ public class JasonLau extends ClubMember implements Hacker, Soviet {
 			}
 		}
 		System.out.println("jason shook hands with" + getHandshakes());
-		
-		for(Student s: arr){
-			if(s instanceof Soviet){
-				sovs.add(s);
-			}
-		}
 	}
 	
 	public void run(){
@@ -57,9 +50,15 @@ public class JasonLau extends ClubMember implements Hacker, Soviet {
 		}
 	
 	public void receiveMessage(Message message){
+		ArrayList<Student> sovs = new ArrayList<Student>();
+		for(Student s: arr){
+			if(s instanceof Soviet){
+				sovs.add(s);
+			}
+		}
 		
 		message.pass(this,sovs.get(sovs.size()-1));
-		sovs.remove(sovs.size()-1);
+		sovs.remove(0);
 		
 		
 		
