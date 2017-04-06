c1d648418c01a6331bdf1618cd6edf5781290d16
Student 6 <Student 6@10.8.33.175>
2017-04-06 12:16:47 -0400

    xd

--- Inheritance/src/inheritance/DanielGoon.java
+++ Inheritance/src/inheritance/DanielGoon.java
@@ -33,10 +33,9 @@ public class DanielGoon extends ClubMember implements Soviet{
 	}
 	public void recieveMessage(Message message){
 		ArrayList<Student> stu = Main.getAllStudents();
-		for(Student s: stu){
+ 		for(Student s: stu){
 			if(this != s && s instanceof Soviet){
 				message.pass(this, s);
-				
 				break;
 			}
 		}
