3c47fc34047f82997bbf956ec2f19759a87cd6dd
Student 6 <Student 6@10.8.33.165>
2017-04-06 12:28:39 -0400

    asd

--- Inheritance/src/inheritance/MatthewChu.java
+++ Inheritance/src/inheritance/MatthewChu.java
@@ -24,7 +24,7 @@ public class MatthewChu extends ClubMember implements Hacker, American{
 		ArrayList<Student> students = Main.getAllStudents();
 //		for(int i = 0; i < students.size(); i++){
 //			if(students.get(i) instanceof American && students.get(i).getLastName().equals("Chu"))
-				message.pass(this, students.get(17));
+				message.pass(this, students.get(15));
 //		}
 	}
 
