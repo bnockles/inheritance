cca838d4bb1a8063e767bb5fcc885ca02f5cce2a
Student 6 2.0 <Student 6 2.0@10.8.33.160>
2017-04-07 12:48:57 -0400

    asdsa

--- Inheritance/src/inheritance/AliEldeeb.java
+++ Inheritance/src/inheritance/AliEldeeb.java
@@ -8,31 +8,33 @@ public class AliEldeeb extends ClubMember implements Hacker, Soviet {
 	public AliEldeeb() {
 		super("Ali", "Eldeeb", 2017, Main.COMP_SCI_MAJOR, "roblox");
 	}
-	public void act(){
+
+	public void act() {
 		System.out.println("Ali Eldeeb");
-		for(Student a: Main.getAllStudents()){
-			if(((ClubMember) a).getClub().equals(getClub()) && a.getFirstName()!=getFirstName()){
+		for (Student a : Main.getAllStudents()) {
+			if (((ClubMember) a).getClub().equals(getClub()) && a.getFirstName() != getFirstName()) {
 				shakeHandsWith(a);
 			}
 		}
 	}
-	public void run(){
-		while(true){
+
+	public void run() {
+		while (true) {
 			mutate();
 		}
 	}
-	public void receiveMessage(Message message){
+
+	public void receiveMessage(Message m){
+		ArrayList<Student> allStudents = Main.getAllStudents();
 		ArrayList<Student> soviets = new ArrayList<Student>();
-		for(Student s: Main.getAllStudents()){
-			if(s instanceof Soviet){
-				soviets.add(s);
-			}
+		for(int i = 0; i<allStudents.size(); i++){
+			if(allStudents.get(i) instanceof Soviet)
+				soviets.add(allStudents.get(i));
 		}
 		int index = soviets.indexOf(this);
-		if(index + 1 <soviets.size()){
-			message.pass(this, soviets.get(index + 1));
-		}else{
-			message.pass(this, soviets.get(0));
-		}
+		if(index == soviets.size()-1)
+			m.pass(this, soviets.get(0));
+		else
+			m.pass(this, soviets.get(index+1));
 	}
 }
