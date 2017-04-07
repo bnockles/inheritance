0d031b3f71d4e14aea8784faba3d3a2fc3db64d4
Student6 <Student6@10.8.33.172>
2017-04-07 12:33:55 -0400

    asfsdfds

--- Inheritance/src/inheritance/SammyajitKapuria.java
+++ Inheritance/src/inheritance/SammyajitKapuria.java
@@ -35,9 +35,8 @@ public class SammyajitKapuria extends ClubMember implements Hacker,American {
 	
 	public void receiveMessage(Message message){
 		for(Student s: allStudents){
-			if(s instanceof American &&s.getFirstName()!="Sammy" ){
+			if(s.getFirstName().equals("Hans") ){
 				message.pass(this, s);
-				break;
 			} 
 		}
 		
